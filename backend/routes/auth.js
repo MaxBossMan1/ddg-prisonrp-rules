8d525c4cac09aadad393db1781c82d3ae6bb4cfa
Maxime Lou Gauthier-Dumais <max@Maximes-MacBook-Air.local>
2025-06-02 00:02:06 -0400

    Fix Steam authentication by manually building OpenID URL to bypass passport-steam caching

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -235,10 +235,33 @@ router.get('/steam', (req, res, next) => {
         `);
     }
     
-    // Recreate Steam strategy with current dynamic URLs
-    passport.use('steam', createSteamStrategy());
+    // Build Steam OpenID URL manually to bypass passport-steam caching issues
+    const currentUrls = getDynamicUrls();
+    console.log('🔧 Manual Steam URL generation with:', currentUrls);
     
-    passport.authenticate('steam')(req, res, next);
+    const steamOpenIdUrl = 'https://steamcommunity.com/openid/login';
+    const params = new URLSearchParams({
+        'openid.mode': 'checkid_setup',
+        'openid.ns': 'http://specs.openid.net/auth/2.0',
+        'openid.ns.sreg': 'http://openid.net/extensions/sreg/1.1',
+        'openid.sreg.optional': 'nickname,email,fullname,dob,gender,postcode,country,language,timezone',
+        'openid.ns.ax': 'http://openid.net/srv/ax/1.0',
+        'openid.ax.mode': 'fetch_request',
+        'openid.ax.type.fullname': 'http://axschema.org/namePerson',
+        'openid.ax.type.firstname': 'http://axschema.org/namePerson/first',
+        'openid.ax.type.lastname': 'http://axschema.org/namePerson/last',
+        'openid.ax.type.email': 'http://axschema.org/contact/email',
+        'openid.ax.required': 'fullname,firstname,lastname,email',
+        'openid.identity': 'http://specs.openid.net/auth/2.0/identifier_select',
+        'openid.claimed_id': 'http://specs.openid.net/auth/2.0/identifier_select',
+        'openid.return_to': currentUrls.returnURL,
+        'openid.realm': currentUrls.realm
+    });
+    
+    const redirectUrl = `${steamOpenIdUrl}?${params.toString()}`;
+    console.log('🔧 Redirecting to Steam with return URL:', currentUrls.returnURL);
+    
+    res.redirect(redirectUrl);
 });
 
 router.get('/steam/return', 
